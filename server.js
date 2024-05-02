76fbe8bc9a556d44862cf006a1daef29ee7f0218
FREANIC1 <160384095+FREANIC1@users.noreply.github.com>
2024-05-02 17:57:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
-// server.js
-
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
 const PORT = process.env.PORT || 3000;
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    // Listen for incoming messages from clients
-    socket.on('chat message', (msg) => {
-        console.log('Message:', msg);
-
-        // Broadcast the message to all connected clients, including the sender
-        io.emit('chat message', msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
 });
 
-server.listen(PORT, () => {
+// Start the server
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
